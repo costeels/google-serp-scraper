366ee22d66d93dd53fe7794ba9f749c30c8d2dba
Dmitry Kuznetsov <kudmni@gmail.com>
2022-06-29 18:46:43 +0300

    Serp checker added

---
+++ serp/views/new.php
@@ -0,0 +1,44 @@
+<?php require_once('header.php'); ?>
+  <?php if (isset($errors)): ?>
+    <h3>Errors:</h3>
+      <?php foreach ($errors as $error) : ?>
+        <pre><?php echo $error['detail'] ?></pre>
+      <?php endforeach; ?>
+  <?php endif; ?>
+  <form method="post">
+    <div class="mb-3">
+      <label for="keywords" class="form-label">keywords*
+      <textarea class="form-control" name="keywords"><?php echo isset($_POST['keywords']) ? $_POST['keywords'] : '' ?></textarea>
+      </label>
+    </div>
+    <div class="mb-3">
+      <label>googleId
+        <input class="form-control" type="number" name="googleId" value="<?php echo isset($_POST['googleId']) ? $_POST['googleId'] : '' ?>">
+      </label><br>
+    </div>
+    <div class="mb-3">
+      device:
+      <label>desktop
+        <input type="radio" name="device" value="desktop" <?php echo (isset($_POST['device']) ? $_POST['device'] : '') == 'desktop' ? 'checked' : '' ?>>
+      </label>
+      <label>mobile
+        <input type="radio" name="device" value="mobile" <?php echo (isset($_POST['device']) ? $_POST['device'] : '') == 'mobile' ? 'checked' : '' ?>>
+      </label><br>
+    </div>
+    <div class="mb-3">
+      <label>hl
+        <input class="form-control" type="text" name="hl" value="<?php echo isset($_POST['hl']) ? $_POST['hl'] : 'en' ?>">
+      </label><br>
+    </div>
+    <div class="mb-3">
+      noreask:
+      <label>0
+        <input type="radio" name="noreask" value="0" <?php echo (isset($_POST['noreask']) ? $_POST['noreask'] : '') == '0' ? 'checked' : '' ?>>
+      </label>
+      <label>1
+        <input type="radio" name="noreask" value="1" <?php echo (isset($_POST['noreask']) ? $_POST['noreask'] : '') == '1' ? 'checked' : '' ?>>
+      </label><br>
+    </div>
+    <input type="submit" value="Create">
+  </form>
+<?php require_once('footer.php'); ?>
--- serp/views/result.php
+++ serp/views/result.php
@@ -10,19 +10,9 @@
     <h3>Task status</h3>
     <pre><?php print_r($status) ?></pre>
   <?php endif; ?>
-  <?php if (isset($domain)): ?>
-    <h3>Domain</h3>
-    <pre><?php echo $domain ?></pre>
-  <?php endif; ?>
-  <?php if (isset($positions)): ?>
-    <h3>Positions</h3>
-      <?php foreach ($positions as $position) : ?>
-        <?php if ($position['position']): ?>
-          <pre><?php echo $position['query'] ?> => <?php echo $position['position'] ?> (<?php echo $position['url'] ?>)</pre>
-        <?php else: ?>
-          <pre><?php echo $position['query'] ?> => N/A</pre>
-        <?php endif; ?>
-      <?php endforeach; ?>
+  <?php if (isset($results)): ?>
+    <h3>Result</h3>
+    <pre><?php print_r($results) ?></pre>
   <?php elseif (isset($taskId)): ?>
     <form method="get">
       <input type="hidden" name="taskId" value="<?php echo $taskId ?>">
