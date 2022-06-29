36c1b545ac63774dc543eb9e3131e973be1482ad
igor <i.lvov@hacklix.com>
2022-06-29 17:21:14 +0300

    Bootstrap simple design

--- views/new.php
+++ views/new.php
@@ -1,21 +1,22 @@
-<html lang="en">
-  <body>
-    <?php if (isset($errors)): ?>
-      <h3>Errors:</h3>
-        <?php foreach ($errors as $error) : ?>
-          <pre><?php echo $error['detail'] ?></pre>
-        <?php endforeach; ?>
-    <?php endif; ?>
-    <form method="post">
-      <label>domain*:
-        <input type="text" name="domain" value="<?php echo isset($_POST['domain']) ? $_POST['domain'] : '' ?>">
-      </label><br>
-      <label>keywords*:
-        <textarea name="keywords"><?php echo isset($_POST['keywords']) ? $_POST['keywords'] : '' ?></textarea>
-      </label><br>
+<?php require_once('./views/header.php'); ?>
+  <?php if (isset($errors)): ?>
+    <h3>Errors:</h3>
+      <?php foreach ($errors as $error) : ?>
+        <pre><?php echo $error['detail'] ?></pre>
+      <?php endforeach; ?>
+  <?php endif; ?>
+  <form method="post">
+    <div class="mb-3">
+      <label for="keywords" class="form-label">Keywords*</label>
+      <textarea id="keywords" class="form-control" name="keywords"><?php echo isset($_POST['keywords']) ? $_POST['keywords'] : '' ?></textarea>
+    </div>
+
+    <div class="mb-3">
       <label>googleId:
-        <input type="number" name="googleId" value="<?php echo isset($_POST['googleId']) ? $_POST['googleId'] : '' ?>">
+        <input class="form-control" type="number" id="googleId" name="googleId" value="<?php echo isset($_POST['googleId']) ? $_POST['googleId'] : '' ?>">
       </label><br>
+    </div>
+    <div class="mb-3">
       device:
       <label>desktop
         <input type="radio" name="device" value="desktop" <?php echo (isset($_POST['device']) ? $_POST['device'] : '') == 'desktop' ? 'checked' : '' ?>>
@@ -23,9 +24,13 @@
       <label>mobile
         <input type="radio" name="device" value="mobile" <?php echo (isset($_POST['device']) ? $_POST['device'] : '') == 'mobile' ? 'checked' : '' ?>>
       </label><br>
+    </div>
+    <div class="mb-3">
       <label>hl
-        <input type="text" name="hl" value="<?php echo isset($_POST['hl']) ? $_POST['hl'] : '' ?>">
+        <input class="form-control" type="text" name="hl" value="<?php echo isset($_POST['hl']) ? $_POST['hl'] : 'en' ?>">
       </label><br>
+    </div>
+    <div class="mb-3">
       noreask:
       <label>0
         <input type="radio" name="noreask" value="0" <?php echo (isset($_POST['noreask']) ? $_POST['noreask'] : '') == '0' ? 'checked' : '' ?>>
@@ -33,7 +38,7 @@
       <label>1
         <input type="radio" name="noreask" value="1" <?php echo (isset($_POST['noreask']) ? $_POST['noreask'] : '') == '1' ? 'checked' : '' ?>>
       </label><br>
-      <input type="submit" value="Create">
-    </form>
-  </body>
-</html>
+    </div>
+    <input type="submit" value="Create">
+  </form>
+<?php require_once('./views/footer.php'); ?>
--- views/result.php
+++ views/result.php
@@ -1,33 +1,32 @@
-<html lang="en">
-  <body>
-    <?php if (isset($errors)): ?>
-      <h3>Errors:</h3>
-        <?php foreach ($errors as $error) : ?>
-          <pre><?php echo $error['detail'] ?></pre>
-        <?php endforeach; ?>
-    <?php endif; ?>
-    <?php if (isset($status) && $status['status'] != 'finished'): ?>
-      <h3>Task status:</h3>
-      <pre><?php print_r($status) ?></pre>
-    <?php endif; ?>
-    <?php if (isset($domain)): ?>
-      <h3>Domain:</h3>
-      <pre><?php echo $domain ?></pre>
-    <?php endif; ?>
-    <?php if (isset($positions)): ?>
-      <h3>Positions:</h3>
-        <?php foreach ($positions as $position) : ?>
-          <?php if ($position['position']): ?>
-            <pre><?php echo $position['query'] ?> => <?php echo $position['position'] ?> (<?php echo $position['url'] ?>)</pre>
-          <?php else: ?>
-            <pre><?php echo $position['query'] ?> => N/A</pre>
-          <?php endif; ?>
-        <?php endforeach; ?>
-    <?php elseif (isset($taskId)): ?>
-      <form method="get">
-        <input type="hidden" name="taskId" value="<?php echo $taskId ?>">
-        <input type="submit" value="Update">
-      </form>
-    <?php endif; ?>
-  </body>
-</html>
+<?php require_once('./views/header.php'); ?>
+  <a href="positions.php">← Back</a>
+  <?php if (isset($errors)): ?>
+    <h3>Errors:</h3>
+      <?php foreach ($errors as $error) : ?>
+        <pre><?php echo $error['detail'] ?></pre>
+      <?php endforeach; ?>
+  <?php endif; ?>
+  <?php if (isset($status) && $status['status'] != 'finished'): ?>
+    <h3>Task status:</h3>
+    <pre><?php print_r($status) ?></pre>
+  <?php endif; ?>
+  <?php if (isset($domain)): ?>
+    <h3>Domain:</h3>
+    <pre><?php echo $domain ?></pre>
+  <?php endif; ?>
+  <?php if (isset($positions)): ?>
+    <h3>Positions:</h3>
+      <?php foreach ($positions as $position) : ?>
+        <?php if ($position['position']): ?>
+          <pre><?php echo $position['query'] ?> => <?php echo $position['position'] ?> (<?php echo $position['url'] ?>)</pre>
+        <?php else: ?>
+          <pre><?php echo $position['query'] ?> => N/A</pre>
+        <?php endif; ?>
+      <?php endforeach; ?>
+  <?php elseif (isset($taskId)): ?>
+    <form method="get">
+      <input type="hidden" name="taskId" value="<?php echo $taskId ?>">
+      <input type="submit" value="Update">
+    </form>
+  <?php endif; ?>
+<?php require_once('./views/footer.php'); ?>
